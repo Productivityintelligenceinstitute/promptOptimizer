c7989c0d2ebaebb431eda9c9eea0b872b4c5c988
Raheel1258 <itsraheelahmed1258@gmail.com>
2026-01-05 11:40:39 +0500

    refactor: update API routes for packages and permissions, adding specific endpoints and improving parameter handling feat: implement prompt optimization routes with basic, structure, master, and system level optimizations refactor: streamline package and permission deletion methods to use IDs instead of names chore: remove unused optimization services and related routes

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from apis.routers.basic_level_optimization import basic_level_optimization_router
-from apis.routers.structure_level_optimization import structured_level_optimization_router
-from apis.routers.master_level_optimization import master_level_optimization_router
-from apis.routers.system_level_optimization import system_level_optimization_router
+from apis.routers.prompt_optimization import prompt_optimization_router
 from apis.routers.accounts import accounts_router
 from apis.routers.subscription import subscription_router
 from apis.routers.chat import chat_router
 from apis.routers.library import library_router
 from apis.routers.customer_support_chatbot import customer_support_chatbot_router
-from apis.routers.kb_ingestion import kb_ingestion_router
+from admin.routes.kb_ingestion import kb_ingestion_router
 
 from admin.routes.permissions import permissioons_router
 from admin.routes.packages import packages_router
@@ -38,10 +35,7 @@ add_pagination(app)
 app.include_router(customer_support_chatbot_router, tags=["Customer Support Chatbot"])
 app.include_router(accounts_router, tags=["Accounts"])
 app.include_router(subscription_router, tags=["Subscription"])
-app.include_router(basic_level_optimization_router, tags=["Prompt Optimization"])
-app.include_router(structured_level_optimization_router, tags=["Prompt Optimization"])
-app.include_router(master_level_optimization_router, tags=["Prompt Optimization"])
-app.include_router(system_level_optimization_router, tags=["Prompt Optimization"])
+app.include_router(prompt_optimization_router, tags=["Prompt Optimization"])
 app.include_router(chat_router, tags=["Chat"])
 app.include_router(library_router, tags=["Library"])
 app.include_router(kb_ingestion_router, tags=["Admin - KB Ingestion"])
