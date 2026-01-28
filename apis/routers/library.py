a3a08e2e1b147de22bd4639a55c5c54d98b1aba5
Raheel1258 <itsraheelahmed1258@gmail.com>
2026-01-28 13:21:24 +0500

    fix: update my_library endpoint to include user_id in the path

--- apis/routers/library.py
+++ apis/routers/library.py
@@ -39,7 +39,7 @@ async def get_library(
     user_uuid = _get_user_uuid(user_id, db)
     return get_library_service(user_uuid, db, page, size, q)
 
-@library_router.get("/me")
+@library_router.get("/me/{user_id}")
 async def my_library(
     user_id: str,
     page: int = 1,
